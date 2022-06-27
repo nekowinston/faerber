728732cfc5f08652a0bdba7310cce85eef6524a2
Ace Daksh <ektadaksh7@gmail.com>
2022-06-26 17:28:35 -0700

    Added is_empty()

--- native/src/main.rs
+++ native/src/main.rs
@@ -82,7 +82,7 @@ fn main() {
 
     let flavour;
     // get the first flavour if no flavour is specified
-    if &args.flavour == "" {
+    if &args.flavour.is_empty() {
         flavour = palette.flavours[0].clone();
     } else {
         flavour = palette
