576429707450777d8b6d8cf29b9245e131cd2589
Ace Daksh <ektadaksh7@gmail.com>
2022-06-28 18:57:08 -0700

    We got the basics

--- native/src/gui.rs
+++ native/src/gui.rs
@@ -7,16 +7,18 @@ fn build_ui() -> impl Widget<()> {
     Padding::new(
         10.0,
         Flex::row().with_flex_child(
-            Flex::column().with_flex_child(
-                Align::centered(
-                    Label::new("faerber!").with_font(
-                        FontDescriptor::new(FontFamily::SYSTEM_UI)
-                            .with_weight(FontWeight::BOLD)
-                            .with_size(48.0),
+            Flex::column()
+                .with_flex_child(
+                    Align::centered(
+                        Label::new("faerber!").with_font(
+                            FontDescriptor::new(FontFamily::SYSTEM_UI)
+                                .with_weight(FontWeight::BOLD)
+                                .with_size(48.0),
+                        ),
                     ),
-                ),
-                1.0,
-            ),
+                    1.0,
+                )
+                .with_flex_child(Align::centered(Button::new("Convert!")), 1.0),
             1.0,
         ),
     )
