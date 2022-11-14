c1bda0fb81c45d6df34985e233b8140b1f80193e
winston <hey@winston.sh>
2022-11-15 00:18:14 +0100

    chore: update dependencies

--- benches/main_benchmark.rs
+++ benches/main_benchmark.rs
@@ -1,35 +1,15 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 use deltae::DEMethod;
-use faerber::custom_lab::Lab;
-use faerber::{convert, rgba_pixels_to_labs};
+use faerber::{convert, convert_loop, rgba_pixels_to_labs};
 use image::RgbaImage;
-
-fn convert_image_e76(img: RgbaImage, palette: Vec<Lab>) -> Vec<u8> {
-    return convert(img, DEMethod::DE1976, &palette);
-}
-
-fn convert_image_e94t(img: RgbaImage, palette: Vec<Lab>) -> Vec<u8> {
-    return convert(img, DEMethod::DE1994T, &palette);
-}
-
-fn convert_image_e94g(img: RgbaImage, palette: Vec<Lab>) -> Vec<u8> {
-    return convert(img, DEMethod::DE1994G, &palette);
-}
-
-fn convert_image_e2000(img: RgbaImage, palette: Vec<Lab>) -> Vec<u8> {
-    return convert(img, DEMethod::DE2000, &palette);
-}
+use rand;
 
 pub fn benchmark(c: &mut Criterion) {
-    c.sample_size(10);
-
     // benchmark image: Wanderer über dem Nebelmeer - by Casper David Friedrich
     let img: RgbaImage = image::open("./benches/wanderer-ueber-dem-nebelmeer.jpg")
         .expect("Benchmark image should exist")
         .to_rgba8();
-
-    let img_pixels = img.pixels();
 
     // benchmark colorscheme: Nord - by Arctic Ice Studio
     let colors: &[u32] = &[
@@ -37,26 +17,48 @@ pub fn benchmark(c: &mut Criterion) {
         0xECEFF4, 0xE5E9F0, 0xD8DEE9, 0x4C566B, 0x434C5F, 0x3B4253, 0x2E3440,
     ];
 
+    let random_pixel = img
+        .pixels()
+        .nth(rand::random::<usize>() % img.pixels().count())
+        .expect("Image should have at least one pixel");
+    let random_lab = faerber::Lab::from_rgba(&random_pixel.0);
     let palette = faerber::convert_palette_to_lab(colors);
 
-    // deltaE benchmarks
-    c.bench_function("deltaE76", |b| {
-        b.iter(|| convert_image_e76(img.clone(), palette.clone()))
-    });
-    c.bench_function("deltaE94G", |b| {
-        b.iter(|| convert_image_e94g(img.clone(), palette.clone()))
-    });
-    c.bench_function("deltaE94T", |b| {
-        b.iter(|| convert_image_e94t(img.clone(), palette.clone()))
-    });
-    c.bench_function("deltaE2000", |b| {
-        b.iter(|| convert_image_e2000(img.clone(), palette.clone()))
-    });
+    c.benchmark_group("pixel")
+        .sample_size(100)
+        .bench_function("de1976", |b| {
+            b.iter(|| convert_loop(DEMethod::DE1976, &palette, &random_lab))
+        })
+        .bench_function("de1994g", |b| {
+            b.iter(|| convert_loop(DEMethod::DE1994G, &palette, &random_lab))
+        })
+        .bench_function("de1994t", |b| {
+            b.iter(|| convert_loop(DEMethod::DE1994T, &palette, &random_lab))
+        })
+        .bench_function("de2000", |b| {
+            b.iter(|| convert_loop(DEMethod::DE2000, &palette, &random_lab))
+        });
 
-    // all the other operations
-    c.bench_function("rgba_pixel_to_lab", |b| {
-        b.iter(|| rgba_pixels_to_labs(img_pixels.clone()))
-    });
+    c.benchmark_group("image")
+        .sample_size(10)
+        .bench_function("de1976", |b| {
+            b.iter(|| convert(img.clone(), DEMethod::DE1976, &palette))
+        })
+        .bench_function("de1994g", |b| {
+            b.iter(|| convert(img.clone(), DEMethod::DE1994G, &palette))
+        })
+        .bench_function("de1994t", |b| {
+            b.iter(|| convert(img.clone(), DEMethod::DE1994T, &palette))
+        })
+        .bench_function("de2000", |b| {
+            b.iter(|| convert(img.clone(), DEMethod::DE2000, &palette))
+        });
+
+    c.benchmark_group("other")
+        .sample_size(10)
+        .bench_function("rgba_pixels_to_lab", |b| {
+            b.iter(|| rgba_pixels_to_labs(img.pixels().clone()))
+        });
 }
 
 criterion_group!(benches, benchmark);
