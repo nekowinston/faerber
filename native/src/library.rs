87e49e04163e6885d02bb296e58d0773199fdf6b
winston <hey@winston.sh>
2022-12-18 21:27:06 +0100

    fix: actually include libraries at buildtime

--- native/src/library.rs
+++ native/src/library.rs
@@ -1,37 +1,18 @@
-use serde_json::Value;
-use std::collections::HashMap;
-
-pub type Palette = HashMap<String, u32>;
 pub type ColorScheme = HashMap<String, Palette>;
 pub type Library = HashMap<String, ColorScheme>;
+pub type Palette = HashMap<String, u32>;
 use faerber::custom_lab::Lab;
+use serde_json::Value;
+use std::collections::HashMap;
 
 lazy_static::lazy_static! {
     pub static ref LIBRARY: Library = {
         let mut library: Library = HashMap::new();
-
-        let paths = std::fs::read_dir("palettes").unwrap();
-
-        for path in paths {
-            let path = path.unwrap().path();
-            let file = std::fs::read_to_string(&path).unwrap();
-            let name = path.file_stem().unwrap().to_str().unwrap().to_string();
-
-            let json: Value = serde_json::from_str(&file).unwrap();
-            let mut color_scheme: ColorScheme = HashMap::new();
-
-            json.as_object().unwrap().iter().for_each(|(k, v)| {
-                let palette: Palette = v.as_object().unwrap().iter().map(|(k, v)| {
-                    let hex = v.as_str().unwrap().trim_start_matches('#');
-                    (k.to_string(), u32::from_str_radix(hex, 16).unwrap())
-                }).collect();
-
-                color_scheme.insert(k.to_string().replace(' ', "_").to_lowercase(), palette);
-            });
-
-            library.insert(name.to_string().replace(' ', "_").to_lowercase(), color_scheme);
-        }
-
+        library.insert("catppuccin".to_string(), parse_colorscheme(serde_json::from_str(include_str!("../palettes/catppuccin.json")).unwrap()));
+        library.insert("dracula".to_string(), parse_colorscheme(serde_json::from_str(include_str!("../palettes/dracula.json")).unwrap()));
+        library.insert("gruvbox".to_string(), parse_colorscheme(serde_json::from_str(include_str!("../palettes/gruvbox.json")).unwrap()));
+        library.insert("nord".to_string(), parse_colorscheme(serde_json::from_str(include_str!("../palettes/nord.json")).unwrap()));
+        library.insert("solarized".to_string(), parse_colorscheme(serde_json::from_str(include_str!("../palettes/solarized.json")).unwrap()));
         library
     };
 }
@@ -47,4 +28,23 @@ pub fn get_labs(palette: Palette) -> Vec<Lab> {
             ])
         })
         .collect();
+}
+
+pub fn parse_colorscheme(json: Value) -> ColorScheme {
+    let mut color_scheme: ColorScheme = HashMap::new();
+
+    json.as_object().unwrap().iter().for_each(|(k, v)| {
+        let palette: Palette = v
+            .as_object()
+            .unwrap()
+            .iter()
+            .map(|(k, v)| {
+                let hex = v.as_str().unwrap().trim_start_matches('#');
+                (k.to_string(), u32::from_str_radix(hex, 16).unwrap())
+            })
+            .collect();
+
+        color_scheme.insert(k.to_string().replace(' ', "_").to_lowercase(), palette);
+    });
+    color_scheme
 }
