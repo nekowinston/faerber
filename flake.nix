bff98478fd763135798b883db5aeec992a9fdcec
winston <hey@winston.sh>
2023-02-27 14:12:42 +0100

    build: add flake dependencies for macOS

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "faerber - a tool to match your pictures to color palettes";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -18,21 +18,27 @@
           inherit system;
         };
       in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            cargo
-            clippy
-            openssl
-            pkg-config
-            rustc
-            rustfmt
-          ];
-        };
+        devShells.default = let
+          inherit (pkgs.stdenv.hostPlatform) isDarwin;
+        in
+          pkgs.mkShell {
+            buildInputs = with pkgs;
+              [
+                cargo
+                clippy
+                libiconv
+                openssl
+                pkg-config
+                rustc
+                rustfmt
+              ]
+              ++ lib.optionals isDarwin [darwin.apple_sdk.frameworks.Security];
+          };
         packages = rec {
           faerber = pkgs.rustPlatform.buildRustPackage {
             name = "faerber";
             src = ./.;
-            cargoSha256 = "sha256-Y0aefM3e9NnQ10jJEdSr3KgYmQTpt/BJuAQbI8kwPQo=";
+            cargoSha256 = "sha256-HNH2z22sJwJNevbejVDQq9DzG/aPoEJta9LenbKq/H4=";
           };
           default = faerber;
         };
